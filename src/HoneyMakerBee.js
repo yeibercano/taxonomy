6fb40512db64dd4faf88597772160e266dc8d2d6
yeibercano <yeibercano@gmail.com>
2016-03-21 13:46:06 -0700

    Finished HoneyMakerBee and moving on to Subclass Dance

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,18 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
