af5f293db8cb69fe6a3544f2badd2bf31a1c4ea5
yeibercano <yeibercano@gmail.com>
2016-03-21 12:26:45 -0700

    Finished Grub and Bee, understanding subclassing

--- src/Bee.js
+++ src/Bee.js
@@ -1,4 +1,14 @@
 var Bee = function() {
-  console.log('bee');
+  Grub.call(this);
+  Bee.prototype.constructor = Bee;
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+  console.log(this)
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+
+Bee.prototype.job = function() {
+  return 'keep on growing';
+};
--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,4 @@
 var ForagerBee = function() {
+  
 };
 
